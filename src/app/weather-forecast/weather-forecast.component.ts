3be86ddc998edf688b595aad0ac89099b139e0c2
Péter Koppány <koppany.peter@autsoft.hu>
2024-03-10 22:03:41 +0100

    Adding angular material

--- src/app/weather-forecast/weather-forecast.component.ts
+++ src/app/weather-forecast/weather-forecast.component.ts
@@ -1,6 +1,8 @@
 import { DatePipe, NgFor } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { WeatherForecast } from '@app/models/weather-forecast/weather-forecast';
 import { WeatherForecastService } from './weather-forecast.service';
@@ -8,10 +10,10 @@ import { WeatherForecastService } from './weather-forecast.service';
 @Component({
   selector: 'app-weather-forecast',
   standalone: true,
-  imports: [NgFor, HttpClientModule, DatePipe],
+  imports: [NgFor, HttpClientModule, DatePipe, MatButtonModule, MatCardModule],
   providers: [WeatherForecastService],
   templateUrl: './weather-forecast.component.html',
-  styleUrl: './weather-forecast.component.scss'
+  styleUrl: './weather-forecast.component.scss',
 })
 export class WeatherForecastComponent implements OnInit {
   title = 'Weather Forecast';
@@ -20,7 +22,7 @@ export class WeatherForecastComponent implements OnInit {
   constructor(private weatherForecastService: WeatherForecastService) {}
 
   ngOnInit(): void {
-    this.weatherForecastService.getWeatherForecasts().subscribe(forecasts => {
+    this.weatherForecastService.getWeatherForecasts().subscribe((forecasts) => {
       this.weatherForecasts = forecasts;
     });
   }
